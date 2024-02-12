e0f9f775f2f2723fc72bcf7725890e880abde400
sohkey <sohkey.fetch@gmail.com>
2024-02-12 19:10:11 +0100

    remove game

--- src/components/screen_5.vue
+++ src/components/screen_5.vue
@@ -3,11 +3,13 @@ import router from '../router';
 const go = (to) => {
     router.push(to)
 }
+
+// <a v-on:click="go('gamepreview')" class="text-xl text-white font-orbitron cursor-pointer hover:underline">click here</a>
+
 </script>
 
 <template>
     <div class="h-[50vh] lg:h-screen w-full space-y-10 bg-gradient-to-b from-slate-800 to-slate-900 lg:bg-cyber bg-repeat-round bg-fixed bg-cover flex justify-center items-center flex-col">
-        <h1 class="text-6xl font-orbitron text-slate-100">Game Preview</h1>
-        <a v-on:click="go('gamepreview')" class="text-xl text-white font-orbitron cursor-pointer hover:underline">click here</a>
+        <h1 class="text-6xl font-orbitron text-slate-100">Coming soon...</h1>
     </div>
 </template>
